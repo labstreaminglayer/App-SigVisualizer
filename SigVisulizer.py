6e185a1e41d36e94df17af60059de26b87591f6c
Yida Lin <yida.lin@outlook.com>
2018-07-12 19:27:35 -0400

    Button

--- SigVisulizer.py
+++ SigVisulizer.py
@@ -1,12 +1,13 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
  
-class App(QMainWindow):
+class App(QWidget):
  
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 status bar example - pythonspot.com'
+        self.title = 'PyQt5 button - pythonspot.com'
         self.left = 10
         self.top = 50
         self.width = 1800
@@ -16,8 +17,17 @@ class App(QMainWindow):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        self.statusBar().showMessage('Message in statusbar.')
+ 
+        button = QPushButton('PyQt5 button', self)
+        button.setToolTip('This is an example button')
+        button.move(100,70) 
+        button.clicked.connect(self.on_click)
+ 
         self.show()
+ 
+    @pyqtSlot()
+    def on_click(self):
+        print('PyQt5 button click')
  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
