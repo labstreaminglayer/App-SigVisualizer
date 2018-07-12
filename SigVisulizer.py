8f4ea64a6c91b2056daf5e0167c13a8f713a101d
Yida Lin <yida.lin@outlook.com>
2018-07-12 19:23:53 -0400

    status bar

--- SigVisulizer.py
+++ SigVisulizer.py
@@ -1,21 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 12 19:12:03 2018
-
-@author: Yida Lin
-"""
-
-
-
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5.QtGui import QIcon
  
-class App(QWidget):
+class App(QMainWindow):
  
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 simple window - pythonspot.com'
+        self.title = 'PyQt5 status bar example - pythonspot.com'
         self.left = 10
         self.top = 50
         self.width = 1800
@@ -25,11 +16,9 @@ class App(QWidget):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.statusBar().showMessage('Message in statusbar.')
         self.show()
  
-    
-    
-    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
