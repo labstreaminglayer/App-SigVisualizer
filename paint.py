75d5905e7d77edf14043f9617d6e1236c572ac2b
Yida Lin <yida.lin@outlook.com>
2018-07-14 02:42:11 -0400

    First working version

--- paint.py
+++ paint.py
@@ -5,12 +5,13 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 import random
 from math import sin
+import time
 
 class App(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt paint - pythonspot.com'
+        self.title = 'PySigVisualizer'
         self.left = 10
         self.top = 20
         self.width = 1800
@@ -39,37 +40,53 @@ class PaintWidget(QWidget):
 
 
     # def __init__(self):
-    x1 = 10
-    x2 = 10
-    y1 = 1
-    y2 = 1
-    xlist = list(range(1000))
-    ylist = [None]*1000
-    for i in range(1000):
-        ylist[i] = sin(xlist[i])
+    # x1 = 10
+    # x2 = 10
+    # y1 = 1
+    # y2 = 1
+    # xlist = list(range(1000))
+    # ylist = [None]*1000
+    # for i in range(1000):
+    #     ylist[i] = sin(xlist[i])
+    # x = 0
+    # y = 0
     x = 0
     y = 0
+    lastX = x
+    lastY = y
+    yOffset = 500
+    xLimit = 5
+    # xlist = list(range(500)*40)
+    # ylist = [None] * 2000
+    # for i in range(2000):
+    #     ylist[i] = sin(xlist[i]) * 20
 
     def paintEvent(self, event):
         qp = QPainter(self)
-
         qp.setPen(Qt.black)
+        size = self.size()
 
-    
-    #   for i in range(1024):
-    #   while True:
-        self.x2 += 1
-        # self.y2 += 2
-#   x = random.randint(1, size.width()-1)
-#   y = random.randint(1, size.height()-1)
-#   qp.drawPoint(x, y)
-        # qp.drawLine(self.x1, self.y1, self.x2, sin(self.x2) * 100)
-        # self.x1 = self.x2
-        # self.y1 = sin(self.x2) * 100
-        for i in range(self.x):
-            qp.drawLine(self.xlist[i] * 10, 200, self.xlist[i+1] * 10, self.ylist[i+1]*100 + 200)
-        self.x += 1
-        self.y += 1
+        self.x = 0
+        self.y = 0
+        self.lastX = self.x
+        self.lastY = self.y
+
+        while self.x < self.xLimit:
+            # self.x2 += 1
+            # for i in range(self.x):
+            qp.drawLine(self.lastX, self.lastY + self.yOffset, self.x, self.y + self.yOffset)
+            self.lastX = self.x
+            self.lastY = self.y
+            self.x = (self.x + 3)
+            self.y = sin(self.x) * 50
+            # time.sleep(0.01)
+
+                # qp.drawLine(self.xlist[i] * 10 % size.width(), self.ylist[i]*20 + 200, self.xlist[i+1] * 10 % size.width(), self.ylist[i+1]*20 + 200)
+                # self.x += 1
+                # self.y += 1
+        self.xLimit += 1
+        if self.xLimit >= size.width():
+            self.xLimit %= size.width()
         self.update()
 
 if __name__ == '__main__':
