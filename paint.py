85c6b2713f9645de45346048e3fe1f442fc2dcfe
Yida Lin <yida.lin@outlook.com>
2018-07-16 00:29:31 -0400

    Added Effective srate and y margin

--- paint.py
+++ paint.py
@@ -41,8 +41,9 @@ class App(QMainWindow):
 
 class PaintWidget(QWidget):
     xMargin = 100
+    yMargin = 50
     idx = 0
-    increment = 1
+    chunkSize = 100
 
     # first resolve an EEG stream on the lab network
     print("looking for an EEG stream...")
@@ -53,31 +54,43 @@ class PaintWidget(QWidget):
     inlet = StreamInlet(streams[0])
     samplingRate = int(streams[0].nominal_srate())
     channelCount = int(streams[0].channel_count())
-    timeStampsBuffer = np.zeros(shape=(channelCount,samplingRate))
-    dataBuffer = np.zeros(shape=(channelCount,samplingRate))
-    scaling = 10
+    samplesPerScreen = 400
+    timeStampsBuffer = np.zeros(shape=(channelCount, samplesPerScreen))
+    dataBuffer = np.zeros(shape=(channelCount, samplesPerScreen))
+    yScaling = 10
+    xScaling = 1600 // samplesPerScreen
 
     def paintEvent(self, event):
         qp = QPainter(self)
         qp.setPen(Qt.blue)
+        channelHeight = (self.size().height() - 2 * self.yMargin) / self.channelCount
         self.yOffset = self.size().height() / self.channelCount / 2
-        channelHeight = self.size().height() / self.channelCount
+        self.signalPanelWidth = self.size().width() - 2 * self.xMargin
+        self.signalPanelHeight = self.size().height()
 
         # get a new sample (you can also omit the timestamp part if you're not
         # interested in it)
-        chunk, timestamps = self.inlet.pull_chunk(max_samples=self.samplingRate//4)
+        chunk, timestamps = self.inlet.pull_chunk(max_samples=self.chunkSize)
         if timestamps:
+
+            effectiveFS = (timestamps[-1] - timestamps[0]) / (len(timestamps) - 1)
+            qp.drawText(100, self.channelCount * channelHeight + self.yOffset + self.yMargin, 
+            'Effective sampling rate: {0:.2f}Hz'.format(round(1 / effectiveFS)))
+
             for c in range(len(timestamps)):
-                self.idx = (self.idx + self.increment) % self.dataBuffer.shape[1]
+                self.idx = (self.idx + 1) % self.samplesPerScreen
+                
                 for m in range(self.channelCount):
-                    self.dataBuffer[m, self.idx] = chunk[c][m] * self.scaling
+                    self.dataBuffer[m, self.idx] = chunk[c][m] * self.yScaling
 
         for m in range(self.channelCount):
-            qp.drawText(10, m * channelHeight + self.yOffset, 'Channel {}'.format(m+1))
+            qp.drawText(10, m * channelHeight + self.yOffset + self.yMargin, 'Channel {}'.format(m+1))
 
             for k in range(self.dataBuffer.shape[1]-1):
-                qp.drawLine(k * 4 + self.xMargin, self.dataBuffer[m, k] + m * channelHeight + self.yOffset / 2, 
-                (k+1)*4 + self.xMargin, self.dataBuffer[m, k+1] + m * channelHeight + self.yOffset / 2)
+                qp.drawLine(k * self.xScaling + self.xMargin, 
+                self.dataBuffer[m, k] + m * channelHeight + self.yOffset / 2 + self.yMargin, 
+                (k+1)*self.xScaling + self.xMargin, 
+                self.dataBuffer[m, k+1] + m * channelHeight + self.yOffset / 2 + self.yMargin)
 
         self.update()
 
